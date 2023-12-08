a9283ec0fe1c26ebc6195ccfe061437d5d0c73f3
Hasib Hossain <boraborhasib@gmail.com>
2023-12-08 22:39:19 +0600

    Updated

--- src/app/(root-home)/page.tsx
+++ src/app/(root-home)/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import HomePage from '../components/Home'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello</h1>
+    <main>
+      <HomePage />
     </main>
   )
 }
